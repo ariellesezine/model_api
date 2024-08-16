e3ff78b4db28fac96b4a7de2ab3205890b0b257a
Sezine <ariellesezine90@gmail.com>
2024-08-16 16:52:32 +0100

    changed the tensorflow version

--- radiation_prediction/settings.py
+++ radiation_prediction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['model-api-qp79.onrender.com']
 
 
 # Application definition
